c656845ead08090afab0a640aded1836a7984007
OsssGhoul <osamalghoul2@gmail.com>
2025-05-10 15:13:41 +0300

    Fix some styling

--- FRONT-END/app/page.tsx
+++ FRONT-END/app/page.tsx
@@ -3,9 +3,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      HELLO WORLD
-      <Link href="/sgin-up">Sgin Up</Link>
-    </div>
+    <>
+      <div className={styles.page}>
+        HELLO WORLD
+        <Link href="/sgin-up">Sgin Up</Link>
+        <Link href="/map">Map</Link>
+      </div>
+    </>
   );
 }
