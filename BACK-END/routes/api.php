27adcf3c592b7b3a21f5029441db9a19ca314729
MaiOsamaALMoqayad <maiosama338@gmail.com>
2025-05-30 17:39:24 +0300

    Finish all store features for seller (create, update, show, delete)

--- BACK-END/routes/api.php
+++ BACK-END/routes/api.php
@@ -1,13 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Seller\StoreController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\StoreController;
 
 Route::post('/register', [RegisteredUserController::class, 'store']);
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
@@ -23,8 +23,23 @@ Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
 ->middleware(['auth:sanctum', 'signed', 'throttle:6,1'])
     ->name('verification.verify');
 
+Route::middleware(['auth:sanctum'])->prefix('seller')->group(function () {
 
+    // المتجر
+    Route::post('/store', [StoreController::class, 'store']); // إنشاء المتجر
+    Route::get('/store', [StoreController::class, 'show']);   // عرض المتجر
+    Route::put('/store', [StoreController::class, 'update']);
+    Route::delete('/store', [StoreController::class, 'destroy']);
 
+});    // Route::post('/store', [StoreController::class, 'store']);
+    // Route::put('/store', [StoreController::class, 'update']);
+
+//     // المنتجات
+//     Route::get('/products', [ProductController::class, 'index']);
+//     Route::post('/products', [ProductController::class, 'store']);
+//     Route::put('/products/{id}', [ProductController::class, 'update']);
+//     Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+// });
 
 
 
