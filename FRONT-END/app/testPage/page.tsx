2ee7e362f55753ec5638813523bcca2865d4acb4
OsssGhoul <osamalghoul2@gmail.com>
2025-06-01 16:57:39 +0300

    added test file

--- FRONT-END/app/testPage/page.tsx
+++ FRONT-END/app/testPage/page.tsx
@@ -1,48 +1,50 @@
-'use client';
-import React, { useEffect, useState } from 'react';
+'use client'
 
-function FetchData() {
-  const [data, setData] = useState(null);
+import { useEffect, useState } from 'react';
+
+function Stores() {
+  const [stores, setStores] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch('http://127.0.0.1:8000/api/register')
+    fetch('http://127.0.0.1:8000/api/guest/stores')
       .then(response => {
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
         return response.json();
       })
-      .then(actualData => {
-        setData(actualData);
-        setError(null);
+      .then(data => {
+        setStores(data);
+        setLoading(false);
       })
-      .catch(err => {
-        setError(err.message);
-        setData(null);
-      })
-      .finally(() => setLoading(false));
-  }, []); 
+      .catch(error => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
 
   if (loading) {
-    return <p>Loading data...</p>;
+    return <p>Loading stores...</p>;
   }
 
   if (error) {
-    return <p>Error fetching data: {error}</p>;
+    return <p>Error loading stores: {error.message}</p>;
   }
 
-  if (data) {
-    return (
-      <div>
-        <h1>Data from Backend:</h1>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      </div>
-    );
-  }
-
-  return null;
+  return (
+    <div>
+      <h1>Stores</h1>
+      <ul>
+        {stores.map(store => (
+          <li key={store.id}>
+            {store.name} - {store.address}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default FetchData;
+export default Stores;
