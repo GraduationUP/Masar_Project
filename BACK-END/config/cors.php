b5946376a092a9279eca6c5216f5cf1609922069
MaiOsamaALMoqayad <maiosama338@gmail.com>
2025-07-07 14:06:56 +0300

    cors file

--- BACK-END/config/cors.php
+++ BACK-END/config/cors.php
@@ -1,12 +1,17 @@
 <?php
-
 return [
-    'paths' => ['api/*'],
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
-    'allowed_origins_patterns' => [],
+
+    'allowed_origins' => ['http://localhost:3000'], 
+
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
+
     'max_age' => 0,
-    'supports_credentials' => false,
+
+    'supports_credentials' => true,
 ];
