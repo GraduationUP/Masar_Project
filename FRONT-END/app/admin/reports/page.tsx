01dc6ee339d04e87cb23c165bbc97a62256690d9
OsssGhoul <osamalghoul2@gmail.com>
2025-08-02 14:44:14 +0300

    intilize admin reports page

--- FRONT-END/app/admin/reports/page.tsx
+++ FRONT-END/app/admin/reports/page.tsx
@@ -1,13 +1,45 @@
+"use client";
+
 import ReportSection from "@/components/admin/ReportSection";
 import Header from "@/components/main_layout/header";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import Image from "next/image";
 
 export default function reportsPage() {
   return (
     <>
       <Header />
-        <ReportSection title="معلقة">.</ReportSection>
-        <ReportSection title="جار العمل عليها">.</ReportSection>
-        <ReportSection title="تم حلها">.</ReportSection>
+      <Button onClick={() => window.history.back()} className="rounded-full">
+        <ArrowRight />
+      </Button>
+      <ReportSection title="معلقة">
+        <Image
+          src={"/admin/pending_report.svg"}
+          alt="pending report"
+          width={200}
+          height={200}
+          className={"m-auto"}
+        />
+      </ReportSection>
+      <ReportSection title="جار العمل عليها">
+        <Image
+          src={"/admin/in_progress_report.svg"}
+          alt="in progress report"
+          width={200}
+          height={200}
+          className={"m-auto"}
+        />
+      </ReportSection>
+      <ReportSection title="تم حلها">
+        <Image
+          src={"/admin/done_report.svg"}
+          alt="done report"
+          width={200}
+          height={200}
+          className={"m-auto"}
+        />
+      </ReportSection>
     </>
   );
 }
